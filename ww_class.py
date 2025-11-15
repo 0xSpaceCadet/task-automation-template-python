6e12acd61c5295a0e28577fdd13900f7245ceabe
Kunal Sharma <sskunall@amazon.com>
2025-11-15 21:51:39 +0530

    Oops starting

--- ww_class.py
+++ ww_class.py
@@ -1,10 +1,18 @@
 from ezdxf.xref import detach
-class SnAEmp: # pass
-    def __init__(self, name, level):
+
+
+class SnAEmp:  # pass
+    raise_percent = 40
+    batch_size = 0
+
+    def __init__(self, name, level, ):
         self.name = name
         self.level = level
         self.identifier = name + '@' + str(level)
         self.email = name + '@gmail.com'
+        self.pay = 10 if level <= 4 else 15
+
+        SnAEmp.batch_size += 1  # class level not instance
 
     def details(self):
         returnable = ('{} and {} and {} and {}'.format(
@@ -12,9 +20,14 @@ class SnAEmp: # pass
         )
         return returnable
 
+    def raise_rating(self):
+        self.pay = int(self.pay * self.raise_percent // 100)  # instance raise
+        # self.pay = int(self.pay * SnAEmp.raise_percent // 100)  # class raise modular
+
+
 emp1 = SnAEmp('gamma', 2)
 print(emp1.email, emp1.name, emp1.identifier, emp1.level)
 print('{}, {}'.format(emp1.email, emp1.name))
 
-print('mimmicking getter method\n', emp1.details()) # from instance
-print('mimmicking getter method\n', SnAEmp.details(emp1)) # from # class
+print('mimmicking getter method\n', emp1.details())  # from instance
+print('mimmicking getter method\n', SnAEmp.details(emp1))  # from # class
